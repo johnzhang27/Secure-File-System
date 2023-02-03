f0f937c3bc4d01d8345f0ce9c75edf31bb883108
jaskim9824 <jaskim9824@gmail.com>
2023-02-03 10:31:21 -0700

    added db

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ HOST = "127.0.0.1"
 PORT = 8000
 
 def setupServer():
+    print("Setting up server")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,15 +18,18 @@ def runServer(socket):
 
 def handleRequest(conn, addr):
     with conn: 
-        rec_data = ""
+        rec_data = b""
         while True:
             data = conn.recv(4096)
             if not data:
                 break
             rec_data += data
         parseRecData(rec_data)
+    return
     
 def parseRecData(rec_data):
+    rec_string = rec_data.decode()
+    print(rec_string)
     return
 
 def main():
@@ -34,3 +38,9 @@ def main():
         runServer(socket)
     except:
         print("Exception!")
+
+def test_main():
+    socket = setupServer()
+    runServer(socket)
+
+test_main()
